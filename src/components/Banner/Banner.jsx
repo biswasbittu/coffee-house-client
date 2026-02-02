35850ff12c061a6c9271e8ca505e9ec0b34afb9a
Bittu Biswas <biswasbittu7415@yahoo.com>
2026-02-02 13:21:39 +0530

    Banner section design done

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import main_bg from "../../assets/images/more/3.png"
+import i1 from "../../assets/images/icons/1.png"
+import i2 from "../../assets/images/icons/2.png"
+import i3 from "../../assets/images/icons/3.png"
+import i4 from "../../assets/images/icons/4.png"
+
 const Banner = () => {
     return (
         <div>
@@ -21,14 +26,35 @@ const Banner = () => {
                                 Get the nostalgia back!! Your companion of every moment!!! 
                                 Enjoy the beautiful moments and make them memorable..
                             </p>
-                            <button className="btn rounded-0 bg-[#E3B577] text-black font-rancho">Get Started</button>
+                            <button className="btn rounded-0 bg-[#E3B577] font-normal text-xl text-black font-rancho">Learn More</button>
                         </div>
                     </div>
                 </div>
-            </section>
+            </section >
             {/* icon section */}
-            <section>
-
+            <section className='bg-[#ECEAE3] '>
+                 <div className='max-w-7xl mx-auto flex flex-col md:flex-row justify-center items-center md:justify-between px-2 '>
+                    <div className='py-4 space-y-2 '>
+                         <img src={i1} alt="" />
+                         <h1 className='text-4xl font-rancho'>Awesome Aroma</h1>
+                         <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                    </div>
+                    <div className='py-4 space-y-2'>
+                         <img src={i2} alt="" />
+                         <h1 className='text-4xl font-rancho'>High Quality</h1>
+                         <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                    </div>
+                    <div className='py-4 space-y-2'>
+                         <img src={i3} alt="" />
+                         <h1 className='text-4xl font-rancho'>Pure Grades</h1>
+                         <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                    </div>
+                    <div className='py-4 space-y-2'>
+                         <img src={i4} alt="" />
+                         <h1 className='text-4xl font-rancho'>Proper Roasting</h1>
+                         <p>You will definitely be a fan of the design & aroma of your coffee</p>
+                    </div>
+                 </div>
             </section>
         </div>
     );
